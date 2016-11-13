222475974afc4783dfecca3d767ceb5c8dc33aba
ALiangLiang <david27072183@gmail.com>
2016-11-14 03:27:08 +0800

    INIT: initial commit on gh-pages branch

---
+++ release.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+
+function changeName(name) {
+  return name.replace(/ /g, '.').replace(/[\W]/g, '.').replace(/\.+/g, '.')
+}
+
+const base = 'dist/';
+fs.readFile(base + 'latest.yml', {
+  encoding: 'utf8'
+}, (err, data) => {
+  const
+    raw = data.split('\n'),
+    originName = raw[1].replace('path: ', ''),
+    newName = changeName(originName);
+  fs.rename(base + originName, base + newName, () => {
+    raw[1] = 'path: ' + newName;
+    const content = raw.join('\n');
+    fs.writeFile(base + 'latest.yml', content, () => console.log(content));
+  })
+});
