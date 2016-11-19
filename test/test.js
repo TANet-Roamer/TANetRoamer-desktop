194805f9898ef40665b9c6d853883fe45797747a
ALiangLiang <david27072183@gmail.com>
2016-11-20 01:56:46 +0800

    Add test scripts

---
+++ test/test.js
@@ -0,0 +1,106 @@
+/**
+ * 這是一個測試檔案，測試流程如下：
+ * 1. 在所有測試開始前，建立測試伺服器，port 為 8012。
+ * 2. 逐一測試各種情況。
+ */
+
+const
+  Application = require('spectron').Application,
+  electron = require('electron'),
+  assert = require('assert'),
+  log = require('npmlog'),
+  server = require('./testServer');
+
+describe('application launch', function() {
+  this.timeout(10000);
+
+  before(function() {
+    return server
+      .then((port) => log.info('Test', `Create test server on port ${port} finish.`));
+  });
+
+  afterEach(function() {
+    if (this.app && this.app.isRunning()) {
+      return this.app.stop();
+    }
+  });
+
+  it('show setting page', function() {
+    this.app = new Application({
+      path: electron,
+      args: ['app', '--enable-logging', '--no-login'],
+    });
+    return this.app.start()
+      .then(() => delay(3000))
+      .then(() => this.app.client.getWindowCount())
+      .then((count) => assert.equal(count, 1)) /* only one fake browser. */
+      .then(() => this.app.mainProcess.openSettingPage())
+      .then(() => this.app.client.getWindowCount())
+      .then((count) => assert.equal(count, 2)) /* one fake browser and one setting page. */
+      .then(() => this.app.client.windowByIndex(1))
+      .then(() => this.app.client.waitUntilWindowLoaded().browserWindow.isVisible())
+      .then((result) => console.log(result))
+      .catch((err) => log.error('Test', err));
+  });
+
+  it('login success', function() {
+    this.app = new Application({
+      path: electron,
+      args: ['app', '--enable-logging', '-l', 'debug', '--user', 'testuser', '--pwd', 'testpwd', '--api-url', 'http://localhost:8012/auth/index.html/u'],
+    });
+    return this.app.start()
+      .then(() => delay(3000))
+      .then(() => this.app.client.getMainProcessLogs())
+      .then((logs) => {
+        for (let log of logs)
+          if (loopMatch(['DEBUG', 'isSuccess', 'true'], log.split(' ')))
+            return Promise.resolve();
+        return Promise.reject();
+      });
+  });
+
+  it('login failed with wrong password', function() {
+    this.app = new Application({
+      path: electron,
+      args: ['app', '--enable-logging', '-l', 'debug', '--user', 'testuser', '--pwd', 'wrong', '--api-url', 'http://localhost:8012/auth/index.html/u'],
+    });
+    return this.app.start()
+      .then(() => delay(3000))
+      .then(() => this.app.client.getMainProcessLogs())
+      .then((logs) => {
+        for (let log of logs)
+          if (loopMatch(['DEBUG', 'isSuccess', 'false'], log.split(' ')))
+            return Promise.resolve();
+        return Promise.reject();
+      });
+  });
+
+  it('login failed with wrong url or SSID', function() {
+    this.app = new Application({
+      path: electron,
+      args: ['app', '--enable-logging', '-l', 'debug', '--user', 'testuser', '--pwd', 'testpwd', '--api-url', 'http://localhost:8012/auth/index.html/wrong'],
+    });
+    return this.app.start()
+      .then(() => delay(3000))
+      .then(() => this.app.client.getMainProcessLogs())
+      .then((logs) => {
+        for (let log of logs)
+          if (loopMatch(['DEBUG', 'isSuccess', 'false'], log.split(' ')))
+            return Promise.resolve();
+        return Promise.reject('wrong url or SSID');
+      });
+  });
+});
+
+function delay(time) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => resolve(), time);
+  });
+}
+
+function loopMatch(arr, target) {
+  for (let i in arr)
+    if (arr[i] !== target[i])
+      return false;
+  return true;
+}
---
+++ test/testServer.js
@@ -0,0 +1,45 @@
+/**
+ * 這個檔案是用來建立一個模擬 TANet Roaming 認證頁面，
+ * 目前是依據彰師大的頁面來製作，
+ * 用來在本地端測試登入器是否能正確執行，
+ * 若有其他不同的地方，例如網址結構，歡迎開 issue 討論。
+ */
+
+const
+  express = require('express'),
+  app = express(),
+  bodyParser = require('body-parser'),
+  url = require('url'),
+  multer = require('multer'),
+  log = require('npmlog'),
+  upload = multer();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+function setReturnTo(req, res, next) {
+  const returnTo = req.get('Referer');
+  if (returnTo)
+    req.session.returnTo = returnTo;
+  next();
+}
+app.get('/auth/index.html/u', function(req, res) {
+  res.send('OK');
+});
+app.post('/auth/index.html/u', function(req, res) {
+  log.info('Test server', 'receive data form: %j', req.body);
+  if (req.body.user === 'testuser' && req.body.password === 'testpwd')
+    res.redirect('https://www.google.com');
+  else
+    res.redirect('/auth/index.html/u?errmsg=Authentication failed');
+});
+
+const port = 8012;
+module.exports = new Promise(function(resolve, reject) {
+  app.listen(port, function() {
+    log.info('Test server', `Create server on port ${port}.`);
+    resolve(port);
+  });
+});
